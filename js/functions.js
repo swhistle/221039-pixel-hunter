b5680e7f19a86c580404fac5ed83593826759a20
Alexander Svistunov <alexsvistunov73@gmail.com>
2018-06-06 01:46:39 +0300

    moved all functions from main in functions.js; put all templates into separate modules and cleaned index.html

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+const getElementFromTemplate = (content) => {
+  const elem = document.createElement(`div`);
+  elem.innerHTML = content;
+  return elem;
+};
+
+const container = document.querySelector(`.central`);
+
+const showScreen = (screen) => {
+  container.innerHTML = ``;
+  container.appendChild(screen);
+};
+
+export {getElementFromTemplate, showScreen};
