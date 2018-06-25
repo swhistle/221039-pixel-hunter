585fbe1fa489b082925ea3932fc3415ac2ceb41f
Alexander Svistunov <alexsvistunov73@gmail.com>
2018-06-26 00:24:13 +0300

    added AbstractView and IntroView

--- js/templates/intro.js
+++ js/templates/intro.js
@@ -1,20 +1,14 @@
 import {getElementFromTemplate, showScreen, container} from '../functions';
 import greeting from './greeting';
+import IntroView from '../view/intro-view';
 
 const moduleIntro = () => {
-  const intro = getElementFromTemplate(`
-  <div id="main" class="central__content">
-    <div id="intro" class="intro">
-      <h1 class="intro__asterisk">*</h1>
-      <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
-    </div>
-  </div>
-`);
+  const intro = new IntroView();
 
   showScreen(intro);
 
-  const link = container.querySelector(`.intro__asterisk`);
-  link.addEventListener(`click`, () => greeting());
+  // const link = container.querySelector(`.intro__asterisk`);
+  // link.addEventListener(`click`, () => greeting());
 };
 
 export default moduleIntro;
---
+++ js/view/abstract-view.js
@@ -0,0 +1,36 @@
+const render = (html) => {
+  const container = document.createElement(`div`);
+  container.innerHTML = html.trim();
+  return container;
+};
+
+class AbstractView {
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error(`Can't instantiate AbstractView, only concrete one`);
+    }
+  }
+
+  get template() {
+    throw new Error(`Template is required`);
+  }
+
+  get element() {
+    if (this._element) {
+      return this._element;
+    }
+    this._element = this.render();
+    this.bind(this._element);
+    return this._element;
+  }
+
+  render() {
+    return render(this.template);
+  }
+
+  bind(element) {
+    // bind handlers if required
+  }
+}
+
+export default AbstractView;
