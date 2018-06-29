d9a76783a62a52e7e75c9078f77f85e5badbbf34
Alexander Svistunov <alexsvistunov73@gmail.com>
2018-06-29 08:53:40 +0300

    created game-model

---
+++ js/model/game-model.js
@@ -0,0 +1,30 @@
+import {gameStateObject, SCORE, INITIAL_GAME_DATA} from "../data/game-data";
+
+export default class GameModel {
+  constructor(userName) {
+    this.username = userName;
+    this._gameState = gameStateObject;
+  }
+
+  getCurrentLives() {
+    return this._gameState.lives;
+  }
+
+  getScore() {
+    return this._gameState.scores;
+  }
+
+  answerCorrectly() {
+    this._gameState.scores.push(SCORE.correctAnswer);
+  }
+
+  answerWrong() {
+    this._gameState.lives--;
+    this._gameState.scores.push(SCORE.wrongAnswer);
+  }
+
+  startNewGame() {
+    this._gameState.lives = INITIAL_GAME_DATA.lives;
+    this._gameState.scores.splice(0, gameStateObject.scores.length);
+  }
+}
